d1fb4877ad47d9c347f071ca56d3b7395ef97c96
Stephen Henderson <hendstephen@gmail.com>
2017-10-18 10:05:18 -0400

    clean up file structure

--- server/main.js
+++
@@ -1,8 +0,0 @@
-<<<<<<< HEAD
-import '/imports/startup/server';
-=======
-// Server entry point, imports all server code
-
-import '/imports/startup/server';
-import '/imports/startup/both';
->>>>>>> 8b5aabc7a5f0203a0f41696a8d064e5849783053
