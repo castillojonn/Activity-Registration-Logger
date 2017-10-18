88fd879b11529d39839da4220f5f35426c7a11db
Stephen Henderson <hendstephen@gmail.com>
2017-10-18 11:58:44 -0400

    working index and Router (for SPA)

--- imports/startup/client/routes.jsx
+++ imports/startup/client/routes.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
-import { App } from '../../ui/layouts/app.jsx';
-import { Index } from '../../ui/components/index.jsx';
+import { App } from '../../ui/layouts/app';
+import { Index } from '../../ui/components/index';
 
 Meteor.startup( () => {
     render(
         <Router history={ browserHistory }>
             <Route path="/" component={ App }>
-                <IndexRoute name="index" component={ Index } />
+                <IndexRoute component={ Index } />
             </Route>
         </Router>,
         document.getElementById('react-root')
