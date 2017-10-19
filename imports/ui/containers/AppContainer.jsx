6ff782eab85bb3dd9955462304d471509bc0fba6
Stephen Henderson <hendstephen@gmail.com>
2017-10-19 12:23:43 -0400

    cleaned up App dependancies

--- imports/ui/containers/AppContainer.jsx
+++ imports/ui/containers/AppContainer.jsx
@@ -1,12 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 import { withTracker } from 'meteor/react-meteor-data';
-import { Events } from '../../api/events/events';
 import App from '../layouts/App';
 
 export default withTracker(() => {
-    const eventsHandle = Meteor.subscribe('events.all');
-    return {
-        loading: !eventsHandle.ready(),
-        events: Events.find({}).fetch(),
-    };
+    return { };
 })(App);
