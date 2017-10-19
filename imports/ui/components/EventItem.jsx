32b542cb4ee522f41fb1f46aee51acdf7e742854
Stephen Henderson <hendstephen@gmail.com>
2017-10-19 10:27:27 -0400

    Update EventItem.jsx

--- imports/ui/components/EventItem.jsx
+++ imports/ui/components/EventItem.jsx
@@ -1,11 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-// import methods from api
-import {
-    insertEvent,
-} from '../../api/events/methods';
-
 export default class EventItem extends Component {
     constructor(props) {
         super(props);
