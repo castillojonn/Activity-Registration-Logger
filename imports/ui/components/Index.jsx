8033f28b8c4ea6482f70a30abd5a12208c552ce1
Jonnathan Castillo <castiljon@gmail.com>
2017-10-25 15:37:49 -0400

    Change to Index.jsx in order to start a merge conflict.

--- imports/ui/components/Index.jsx
+++ imports/ui/components/Index.jsx
@@ -1,3 +1,8 @@
 import React from 'react';
 
-export const Index = () => <h3>Index</h3>;
+export const Index = () => 
+    
+    <div>
+    <h3>Activity Registration Logger</h3>
+    <p>An app that tracks CME credit hours for approved CME course, seminar, and events.</p>
+    </div>;
