8fcd100d9f1eb72d401312d908a2f5138876a4f8
StewSchrieff <stewartschrieffer@Stewarts-MacBook-Pro.local>
2017-10-25 21:49:05 -0400

    Stewart's big commit

--- imports/ui/components/Index.jsx
+++ imports/ui/components/Index.jsx
@@ -4,5 +4,5 @@ export const Index = () =>
     
     <div>
     <h3>Activity Registration Logger</h3>
-    <p>An app that tracks CME credit hours for approved CME course, seminar, and events.</p>
+    <p>Stewart makes some big important changes</p>
     </div>;
