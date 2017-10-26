ea507f3b238982052ee8887fbbf3ca5bf705d240
StewSchrieff <stewartschrieffer@Stewarts-MacBook-Pro.local>
2017-10-25 21:24:34 -0400

    Here we introduce a Merge conflict by Stewart Schrieffer

--- imports/ui/components/Index.jsx
+++ imports/ui/components/Index.jsx
@@ -1,3 +1,9 @@
 import React from 'react';
 
-export const Index = () => <h3>Index</h3>;
+export const Index = () => 
+
+
+<h3>Stewart's Merge Conflict</h3>
+<div>
+	<p>Here we introduce some conflict</p>
+</div;
