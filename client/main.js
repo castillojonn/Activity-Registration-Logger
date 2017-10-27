cd47f66e3c1e328ef8cd48921f96d7df57321d8f
Stephen Henderson <hendstephen@gmail.com>
2017-10-27 17:06:17 -0400

    Refactor bootstrap. Removed all bootstrap scripts and then added meteor add twbs:bootstrap, meteor npm install --save react-bootstrap.

--- client/main.js
+++ client/main.js
@@ -1,9 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
 import { renderRoutes } from '../imports/startup/client/routes';
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/css/bootstrap-theme.css';
 
 Meteor.startup(() => {
     render(renderRoutes(), document.getElementById('react-root'));
