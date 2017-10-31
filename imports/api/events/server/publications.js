2ca0254f3da82f9d2686661ac50a1a2ffed1b5a0
Stephen Henderson <hendstephen@gmail.com>
2017-10-31 13:46:56 -0400

    added basic google users

--- imports/api/events/server/publications.js
+++ imports/api/events/server/publications.js
@@ -1,6 +1,5 @@
 /* eslint-disable prefer-arrow-callback */
 import { Meteor } from 'meteor/meteor';
-
 import { Events } from '../events';
 
 Meteor.publish('events.all', function allEvents() {
--- imports/ui/components/Index.jsx
+++ imports/ui/components/Index.jsx
@@ -1,12 +1,3 @@
 import React from 'react';
 
-export const Index = () =>
-
-    <div className="container">
-    <h3>Activity Registration Logger</h3>
-
-    <div className="container">
-    <h3>Activity Registration Logger</h3>
-    <p>Stewart Testing to see if his github account links with this commit</p>
-    <p>An app that tracks CME credit hours for approved CME course, seminar, and events</p>
-    </div>;
+export const Index = () => <h3>Index</h3>;
