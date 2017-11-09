f50b9a409c913144a97d14f631a8eb12dcacdb35
Stephen Henderson <hendstephen@gmail.com>
2017-11-09 14:31:05 -0500

    cleaned up events and google api

--- imports/api/events/server/publications.js
+++ imports/api/events/server/publications.js
@@ -1,17 +1,58 @@
-/* eslint-disable prefer-arrow-callback */
 import { Meteor } from 'meteor/meteor';
-//import { Events } from '../events';
+import { _ } from 'meteor/underscore';
+import { HTTP } from 'meteor/http';
 
-Meteor.publish('events.all', function(subreddit) {
-    var self = this;
+const POLL_INTERVAL = 10000;
+Meteor.publish('events', function () {
+    const self = this;
+    const publishedKeys = {};
+    const poll = () => {
+        try {
+            const _user = Meteor.users.findOne(this.userId);
 
-    try {
-        var response = GoogleApi.get('https://www.googleapis.com/calendar/v3/users/me/calendarList/en.usa#holiday@group.v.calendar.google.com', {});
+            if (_user && _user.services && _user.services.google) {
+                var options = {};
+                options.headers = {};
+                options.headers.Authorization = 'Bearer ' + _user.services.google.accessToken;
+            }
+            else {
+                throw new Exception("User's google access token not found");
+            }
 
-        self.added('events', Random.id(), response.data);
-        self.ready();
+            var options = {
+                'headers' : {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + _user.services.google.accessToken,
+                    'X-JavaScript-User-Agent': "Google APIs Explorer"
+                }
+            };
 
-    } catch (error) {
-        console.log(error);
-    }
+            var url = 'https://www.googleapis.com/calendar/v3/calendars/capstone.reg.log@gmail.com/events';
+            var response = HTTP.get(url, options);
+
+            _.each(response.data.items, function(item) {
+                if (publishedKeys[item._id]) {
+                    self.changed('events', item._id, item);
+                } else {
+                    publishedKeys[item._id] = true;
+                    self.added('events', item._id, item);
+                }
+            });
+
+            self.ready();
+
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    // run poll every interval
+    poll();
+    //self.ready();
+
+    const interval = Meteor.setInterval(poll, POLL_INTERVAL);
+
+    self.onStop(() => {
+        Meteor.clearInterval(interval);
+    });
 });
--- imports/startup/server/register-api.js
+++ imports/startup/server/register-api.js
@@ -1,5 +1,2 @@
 import '../../api/events/methods';
 import '../../api/events/server/publications';
-
-//import '../../api/posts/server/publications';
-import '../../api/google/server/publications';
