2007240e6143daa827cbbe140101d28ed4160018
Jonnathan Castillo <castiljon@gmail.com>
2017-11-15 17:56:39 -0500

    Random Commit to allow Pull

--- imports/ui/components/Index.jsx
+++ imports/ui/components/Index.jsx
@@ -5,7 +5,8 @@ export const Index = () =>
     <div class="container">
     <h3>Activity Registration Logger</h3>
 
-    <p>An app that tracks CME credit hours for approved CME course, seminar, and events</p>
+    <p>An app that was supposed to track CME credit hours for approved CME course, seminar, and events</p>
+    <p>Now it just does that and not that. Funny how that happens when clients drop.</p>
 
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
--- imports/ui/components/Navigation.jsx
+++ imports/ui/components/Navigation.jsx
@@ -1,5 +1,22 @@
 import React from 'react';
 import { IndexLink, Link } from 'react-router';
+import BS from 'bootstrap';
+
+/* const navbarInstance = (
+  <BS.Navbar>
+    <BS.Navbar.Header>
+      <BS.Navbar.Brand>
+        <a href="/">Activity Registration Logger</a>
+      </BS.Navbar.Brand>
+    </BS.Navbar.Header>
+    <BS.Nav>
+      <BS.NavItem eventKey={1} href="/newsfeed">Newsfeed</BS.NavItem>
+      <BS.NavItem eventKey={2} href="#">Calendar</BS.NavItem>
+    </BS.Nav>
+  </BS.Navbar>
+);
+
+ReactDOM.render(navbarInstance, mountNode);*/
 
 export const Navigation = () => (
     /* Container to prevent an error.
@@ -12,7 +29,7 @@ export const Navigation = () => (
         <nav class="navbar navbar-expand-lg navbar-light bg-light" >
             <a class="navbar-brand" href="/">Activity Registration Logger</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+                <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <div class="navbar-nav">
@@ -22,13 +39,9 @@ export const Navigation = () => (
                 </div>
             </div>
         </nav>
-        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
     </div>
     
     /**<ul>
         <li><IndexLink to="/" activeClassName="active">Index</IndexLink></li>
-        <li><Link to="/newsfeed">Newsfeed</Link></li>
-    </ul>**/
+        <li><Link to="/newsfeed">Newsfeed</Link></li>**/
 )
