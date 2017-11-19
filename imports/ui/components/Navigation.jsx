15a89a96698a9c46a6a0d4441480eac2a6a85f03
Emily Dunenfeld <pepper789@gmail.com>
2017-11-19 18:00:25 -0500

    Simple Appearance Change

--- imports/ui/components/Navigation.jsx
+++ imports/ui/components/Navigation.jsx
@@ -28,19 +28,22 @@ export const Navigation = () => (
             added like normal.
     */
     <div>
-        <Navbar className="navbar navbar-expand-lg navbar-light bg-light" >
-            <a className="navbar-brand" href="/">Activity Registration Logger</a>
-            <Button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                <Glyphicon glyph="menu-up"></Glyphicon>
-            </Button>
-            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div className="navbar-nav">
-                    <a className="nav-item nav-link" href="/newsfeed">Newsfeed<span className="sr-only">(current)</span></a>
+        <p></p>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+	    <a class="navbar-brand" href="/">Activity Registration Logger</a>
+	    <p></p>
+ 	    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                 <Glyphicon glyph="menu-up"></Glyphicon>
+
+	    </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-item nav-link" href="/newsfeed">Newsfeed<span className="sr-only">(current)</span></a>
                     <p></p>
-                    <a className="nav-item nav-link" href="#">Calendar</a>
+                    <a class="nav-item nav-link" href="#">Calendar</a>
                 </div>
             </div>
-        </Navbar>
+        </nav>
     </div>
 
     /**<ul>
