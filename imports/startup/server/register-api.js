f5887f9a5e33726ea86597a7dd3e2b176166ee87
Stephen Henderson <hendstephen@gmail.com>
2017-11-21 09:50:32 -0500

    added attendance api, made calendar id global

--- imports/startup/server/register-api.js
+++ imports/startup/server/register-api.js
@@ -1,2 +1,7 @@
+// events
 import '../../api/events/methods';
 import '../../api/events/server/publications';
+
+// attendance
+import '../../api/attendance/methods';
+import '../../api/attendance/server/publications';
