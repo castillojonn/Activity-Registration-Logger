bd1ad3fe6015b02f7b54cc91e46c0730107e10dc
Drew Campbell <drew.dcampbell@gmail.com>
2017-12-05 14:08:02 -0500

    Update Deploy
    Fixed Google Sign In on Production Environment

--- imports/startup/server/security.js
+++ imports/startup/server/security.js
@@ -2,3 +2,13 @@ import { Meteor } from 'meteor/meteor';
 
 // deny all updates to users' profiles
 Meteor.users.deny({ update: () => true });
+
+ServiceConfiguration.configurations.remove({
+  service: "google"
+});
+ServiceConfiguration.configurations.insert({
+  service: "google",
+  clientId: "1001900628086-kfnvd4q0amdk9koetdcfd3fe7qd9ncki.apps.googleusercontent.com",
+  loginStyle: "popup",
+  secret: "Tu_5D5lR1urWscBWiEF8_J71"
+});
--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-##rm -r ./.deploy
-meteor build .deploy --directory --architecture os.linux.x86_64 --server-only
-cp app.yaml ./.deploy/bundle/app.yaml
-cp package.json ./.deploy/bundle/package.json
-cp Dockerfile ./.deploy/bundle/Dockerfile
-cd ./.deploy/bundle
-gcloud app deploy app.yaml --verbosity=info
