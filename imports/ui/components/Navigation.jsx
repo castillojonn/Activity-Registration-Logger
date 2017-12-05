fc127690ee947159ee0ebb4bf29125b2dd4d9320
Jonnathan Castillo <castiljon@gmail.com>
2017-12-05 09:40:48 -0500

    Navbar overhaul. Now actually using react-bootstrap

--- imports/ui/components/Navigation.jsx
+++ imports/ui/components/Navigation.jsx
@@ -1,32 +1,22 @@
 import React from 'react';
 import { IndexLink, Link } from 'react-router';
 
-import { Button, Navbar, Glyphicon } from 'react-bootstrap';
+import { Button, Navbar, Nav, NavItem, Glyphicon } from 'react-bootstrap';
 
 export const Navigation = () => (
-    /* Container to prevent an error.
-
-        --- Navbar and navigation
-            Using Bootstraps built in Navbar. Functionality can be
-            added like normal.
-    */
-    <div>
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-	    <a className="navbar-brand" href="/">Activity Registration Logger</a>
- 	    {/*<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                 <Glyphicon glyph="menu-up"></Glyphicon>
-
-        </button>*/}
-            {/*<div className="collapse navbar-collapse" id="navbarNavAltMarkup">*/}
-
-                <div className="navbar-nav">
-                    <a className="nav-item nav-link" href="/newsfeed">Newsfeed<span className="sr-only">(current)</span></a> |
-                    <a className="nav-item nav-link" href="/calendar"> Calendar</a> |
-                    <a className="nav-item nav-link" href="/attendance"> Attendance</a>
-                </div>
-            {/*</div>*/}
-
-        </nav>
-    </div>
-
-)
+    <Navbar inverse collapseOnSelect>
+        <Navbar.Header>
+            <Navbar.Brand>
+                <a href="/newsfeed">Activity Registration Logger</a>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+            <Nav pullRight>
+                <NavItem eventKey={1} href="/newsfeed">Newsfeed</NavItem> 
+                <NavItem eventKey={2} href="/calendar">Calendar</NavItem> 
+                <NavItem eventKey={3} href="/attendance">Attendance</NavItem>
+            </Nav>
+        </Navbar.Collapse>
+    </Navbar>
+);
